2363ffd8b5b3593ed8a135188806e576862f6f61
Nathan VanSickle <ndvans03@gmail.com>
2023-08-15 20:02:13 -0400

    create basic PageHeader component

---
+++ odub-frontend/src/components/PageHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <h3>Odub - <em>Overdrive and Under Budget</em></h3>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "PageHeader"
+}
+</script>
+
+<style>
+.header {
+    background-color: #BA0C2F;
+}
+</style>
