abb7f0ae147b2b1f9373c602f8e1263e0411f883
Nathan VanSickle <ndvans03@gmail.com>
2023-08-15 20:05:00 -0400

    add basic header to both views

--- odub-frontend/src/views/AboutPage.vue
+++ odub-frontend/src/views/AboutPage.vue
@@ -1,3 +1,15 @@
 <template>
+    <PageHeader/>
     <h1>About</h1>
 </template>
+
+<script>
+import PageHeader from '@/components/PageHeader.vue'
+
+export default {
+    name: "AboutPage",
+    components: {
+        PageHeader
+    }
+}
+</script>
--- odub-frontend/src/views/HomePage.vue
+++ odub-frontend/src/views/HomePage.vue
@@ -1,6 +1,15 @@
 <template>
+    <PageHeader/>
     <h1>Home</h1>
 </template>
 
 <script>
+import PageHeader from '@/components/PageHeader.vue'
+
+export default {
+    name: "HomePage",
+    components: {
+        PageHeader
+    }
+}
 </script>
