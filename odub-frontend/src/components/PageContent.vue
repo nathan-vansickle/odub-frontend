473c76dde6c5c5d93d64ab0e9b8660b4793d43aa
Nathan VanSickle <ndvans03@gmail.com>
2023-08-17 16:28:04 -0400

    add PageContent component skeleton

---
+++ odub-frontend/src/components/PageContent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="content">
+        <div class="row">
+            <!-- Will use v-for to iterate over an array of objects
+            containing the data necessary to create each card
+            object -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PageContent"
+    }
+</script>
