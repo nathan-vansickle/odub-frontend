74dd6933d2e9e9f30279661b18848b5b04005740
Nathan VanSickle <ndvans03@gmail.com>
2023-08-17 17:15:11 -0400

    improve card style

--- odub-frontend/src/components/ProductCard.vue
+++ odub-frontend/src/components/ProductCard.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="card">
-        <img :src="image" :alt="name" class="product-image">
-        <h3>{{ name }}</h3>
-        <h4>{{ price }}</h4>
-        <p>Buy at <a href="https://google.com">{{ vendor }}</a></p>
+        <img src="@/assets/images/logo.png" :alt="name" class="product-image">
+        <h3 class="product-name">{{ name }}</h3>
+        <h2 class="price">{{ price }}</h2>
+        <p class="link">Buy at <a href="https://google.com">{{ vendor }}</a></p>
     </div>
 </template>
 
@@ -21,8 +21,30 @@ export default {
 </script>
 
 <style>
-card img {
-    height: 500px;
-    width: auto;
+
+.card {
+    border-style: solid;
+    border-width: 2px;
+    border-radius: 15px;
+    width: 220px;
+}
+
+.card img {
+    height: 200px;
+    width: 200px;
+    padding-top: 10px;
+    padding-left: 10px;
+}
+
+.link {
+    padding-left: 10px;
+}
+
+.product-name {
+    padding-left: 10px;
+}
+
+.price {
+    padding-left: 10px;
 }
 </style>
