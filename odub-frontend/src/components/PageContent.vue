d196db6a124423fa43472b51ba50eace200b9530
Nathan VanSickle <ndvans03@gmail.com>
2023-08-17 16:46:44 -0400

    add ProductCard component

--- odub-frontend/src/components/PageContent.vue
+++ odub-frontend/src/components/PageContent.vue
@@ -4,12 +4,16 @@
             <!-- Will use v-for to iterate over an array of objects
             containing the data necessary to create each card
             object -->
+            <ProductCard image="../assets/images/logo.png" name="Test Product" price="$99.99" vendor="GuitarCenter" link="n/a"/>
         </div>
     </div>
 </template>
 
 <script>
+import ProductCard from './ProductCard.vue';
+
     export default {
-        name: "PageContent"
-    }
+    name: "PageContent",
+    components: { ProductCard }
+}
 </script>
---
+++ odub-frontend/src/components/ProductCard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card">
+        <img :src="image" :alt="name" class="product-image">
+        <h3>{{ name }}</h3>
+        <h4>{{ price }}</h4>
+        <p>Buy at <a href="https://google.com">{{ vendor }}</a></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ContentCard",
+    props: {
+        image: String,
+        name: String,
+        price: String,
+        vendor: String,
+        link: String
+    }
+}
+</script>
+
+<style>
+card img {
+    height: 500px;
+    width: auto;
+}
+</style>
